b72465d841697c7a8b3c10b85b0d6296c602cf5d
shoneefd <sfdoerr@berkeley.edu>
2016-04-01 22:20:41 -0700

    Added files via upload

--- unit.py
+++ unit.py
@@ -1,7 +1,16 @@
-class Unit():
+class Unit:
 	def __init__(self, place, name):
 		self.place = place
 		self.name = name
 	def __repr__(self):
 		return self.name
-		
+class Human(Unit):
+	def __init__(self, place, name, health, damage):
+		Unit.__init__(self, place, name)
+		self.health = health
+		self.damage = damage
+	def lose_health(self, hit=1):
+		self.health -= hit
+		if self.health <= 0:
+			self.remove_unit()
+	def remove_unit(self)
