63cd82b82376d5302f289f3cd54fd50bab6ff37b
Ellen Smith <ellen.smith@berkeley.edu>
2016-04-01 22:52:01 -0700

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,16 +1,39 @@
 class Unit:
-	def __init__(self, place, name):
-		self.place = place
-		self.name = name
-	def __repr__(self):
-		return self.name
-class Human(Unit):
-	def __init__(self, place, name, health, damage):
-		Unit.__init__(self, place, name)
+	
+	def __init__(self, health, place = None):
 		self.health = health
-		self.damage = damage
+		self.place = place
+
+	def get_hit(self, damage):
+		self.health -= damage
+
+		if self.health <= 0:
+			game_print('{0} ran out of armor and expired'.format(self))
+			self.place.remove_unit(self)
+
+	def attack(self, hoomans):
+
+	def is_human(self):
+		return False
+
+
+	def __repr__(self):
+		name = type(self).__name__
+		return '{0}{1}, {2})'.format(name, self.health, self.place)
+
+
+class Human(Unit):
+
 	def lose_health(self, hit=1):
 		self.health -= hit
 		if self.health <= 0:
 			self.remove_unit()
-	def remove_unit(self)
+	#def remove_unit(self)
+
+class Squirrel(Unit):
+
+	def attack(self, Human):
+		Human.
+
+
+
